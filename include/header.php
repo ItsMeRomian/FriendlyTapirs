d1aadbee08b0dd09001166ddf1681c966180b835
ItsMeRomian <romian.tairovski@gmail.com>
2019-03-29 16:01:09 +0100

    init

---
+++ include/header.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="icon" href="img/favicon.png" type="image/png">
+	<title>Friendlytapirs</title>
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="vendors/linericon/style.css">
+  <link rel="stylesheet" href="css/themify-icons.css">
+  <link rel="stylesheet" href="vendors/owl-carousel/owl.theme.default.min.css">
+	<link rel="stylesheet" href="vendors/fontawesome/css/all.min.css">
+	<link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
+	<link rel="stylesheet" href="vendors/nice-select/css/nice-select.css">
+	<!-- main css -->
+	<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+	<header class="header_area">
+		<div class="main_menu">
+			<nav class="navbar navbar-expand-lg navbar-light">
+				<div class="container">
+					<a class="navbar-brand logo_h" href="index.html"><img src="img/logo.png" alt=""></a>
+					<a class="navbar-brand logo_inner_page" href="index.html"><img src="img/logo2.png" alt=""></a>      
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+					 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+						<ul class="nav navbar-nav menu_nav">
+							<li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li>
+							<li class="nav-item"><a class="nav-link" href="about.html">Over</a></li>
+							<li class="nav-item"><a class="nav-link" href="portfolio.html">Portfolio</a></li>
+							<li class="nav-item submenu dropdown">
+								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+								 aria-expanded="false">Pages</a>
+								<ul class="dropdown-menu">
+									<li class="nav-item"><a class="nav-link" href="services.html">Diensten</a></li>
+									<li class="nav-item"><a class="nav-link" href="portfolio-details.html">Portfolio Details</a></li>
+								</ul>
+							</li>
+							<li class="nav-item submenu dropdown">
+								<ul class="dropdown-menu">
+									<li class="nav-item"><a class="nav-link" href="blog.html">Blog</a></li>
+									<li class="nav-item"><a class="nav-link" href="single-blog.html">Blog Details</a></li>
+								</ul>
+							</li>
+							<li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</div>
+	</header>
---
+++ index.php
@@ -0,0 +1,436 @@
+<?php include_once('include/header.php'); ?>
+<section class="home_banner_area">
+	<div class="banner_inner">
+		<div class="container">
+			<div class="row align-items-center justify-content-between">
+				<div class="col-lg-6">
+					<div class="banner_content">
+						<h3>FriendlyTapirs</h3>
+						<h1 class="text-uppercase">wij bouwen websites</h1>
+						<h5 class="text-uppercase">Betere bereikbaarheid begint online.</h5>
+						<div class="social_icons my-5">
+							<a href="#"><i class="ti-twitter"></i></a>
+							<a href="#"><i class="ti-skype"></i></a>
+							<a href="#"><i class="ti-instagram"></i></a>
+							<a href="#"><i class="ti-dribbble"></i></a>
+							<a href="#"><i class="ti-vimeo"></i></a>
+						</div>
+						<a class="primary_btn" href="#"><span>Ons werk</span></a>
+					</div>
+				</div>
+				<div class="col-lg-4">
+					<div class="home_right_img">
+						<img class="img-fluid" src="img/banner/home-right.png" alt="">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="statistics_area">
+	<div class="container">
+		<div class="row justify-content-lg-start justify-content-center">
+			<div class="col-lg-2 col-md-3">
+				<div class="statistics_item">
+					<h3><span class="counter">15</span></h3>
+					<p>Blije Klanten</p>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-3">
+				<div class="statistics_item">
+					<h3><span class="counter">12</span>k+</h3>
+					<p>lijnen code</p>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-3">
+				<div class="statistics_item">
+					<h3><span class="counter">8.5</span>/10</h3>
+					<p>gemiddelde score</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!--================ End Statistics Area =================-->
+
+<!--================ Start About Us Area =================-->
+<section class="about_area section_gap">
+	<div class="container">
+		<div class="row justify-content-start align-items-center">
+			<div class="col-lg-5">
+				<div class="about_img">
+					<img class="" src="img/about-us.png" alt="">
+				</div>
+			</div>
+
+			<div class="offset-lg-1 col-lg-5">
+				<div class="main_title text-left">
+					<p class="top_text">Over ons </p>
+					<h2>
+						Wij bouwen moderne websites.<br>
+						
+					</h2>
+					<p>
+						De visie van ons bedrijf is om te zorgen dat wij de online zichtbaarheid van onze klanten maximaliseren. Wij zijn niet alleen gespecialiseerd in websites, maar ook in de optimalisatie van social media, logo's en veel meer.
+					</p>
+					<a class="primary_btn" href="#"><span>Download CV</span></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!--================ End About Us Area =================-->
+
+<!--================ Start Features Area =================-->
+<section class="services_area">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="main_title">
+				<p class="top_text">Diensten </p>
+					<h2>Onze diensten. <br> </h2>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
+				<div class="service_item">
+					<img src="img/services/s1.png" alt="">
+					<h4>Webontwikkeling</h4>
+					<p>De online representatie van uw bedrijf is in dit digitale tijdperk ongelofelijk belangrijk. </p>
+					<a href="#" class="primary_btn2 mt-35">Learn More</a>
+				</div>
+			</div>
+
+			<div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
+				<div class="service_item">
+					<img src="img/services/s2.png" alt="">
+					<h4>Grafisch Ontwerp</h4>
+					<p>De visuele representatie van uw bedrijf is ook van ongekend belang, niet alleen omdat het professioneel oogt. Maar ook omdat je een bepaald beeld wilt koppelen aan je bedrijf. </p>
+					<a href="#" class="primary_btn2 mt-35">Learn More</a>
+				</div>
+			</div>
+
+			<div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
+				<div class="service_item">
+					<img src="img/services/s3.png" alt="">
+					<h4>Domeinregistratie & Webhosting</h4>
+					<p>het adres van uw website is misschien wel het aller belangrijkste voor uw online bereikbaarheid. 
+					<br>
+					De hosting van websites kan soms erg prijzig zijn , wij bij FriendlyTapirs proberen dit zo goedkoop mogelijk aan te bieden.</p>
+					<a href="#" class="primary_btn2 mt-35">Learn More</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!--================ End Features Area =================-->
+
+
+<!--================ Start Testimonial Area =================-->
+<section class="testimonial_area">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="main_title">
+					<p class="top_text">Reviews</p>
+					<h2>Wat vinden onze klanten?<br></h2>
+				</div>
+			</div>
+  </div>
+
+  <div class="owl-carousel owl-theme testimonial-slider ">
+	<div class="testimonial-item">
+	  <div class="row">
+		<div class="col-lg-6">
+		  <div class="testi-img mb-4 mb-lg-0">
+			<img src="img/testimonials/testimonial1.png" alt="">
+		  </div>
+		</div>
+		<div class="col-lg-6">
+		  <div class="testi-right">
+			<h4>Henk Veenstra</h4>
+			<p><small>Eigenaar Autobedrijf Veenstra</small></p>
+
+			<p>In het begin een paar speerpunten meegegeven wat voor ons het belangrijkst was, dit hebben ze goed uitgewerkt en ons het resultaat laten beoordelen. Kleine verbeterpunten werden gelijk aangepast. Al met al super tevreden.</p>
+			<ul class="star_rating mt-4">
+			  <li><span><i class="fas fa-star"></i></span></li>
+			  <li><span><i class="fas fa-star"></i></span></li>
+			  <li><span><i class="fas fa-star"></i></span></li>
+			  <li><span><i class="fas fa-star"></i></span></li>
+			  <li class="disable"><span><i class="fas fa-star"></i></span></li>                  
+			</ul>
+		  </div>
+		</div>
+	  </div>
+	</div>
+
+	<div class="testimonial-item">
+	  <div class="row">
+		<div class="col-lg-6">
+		  <div class="testi-img mb-4 mb-lg-0">
+			<img src="img/testimonials/testimonial1.png" alt="">
+		  </div>
+		</div>
+		<div class="col-lg-6">
+		  <div class="testi-right">
+			<h4>Roser Henrique</h4>
+			<p><small>Project Manager, Apple</small></p>
+
+			<p>Waters can not replenish hath fly and be to brought isn't very days behold without land every above lights us fruitful wherein divide it him fowl moving may beginning subdue fly waters can't replenish hath fly and be to brought isn't very days behold </p>
+			<ul class="star_rating mt-3">
+			  <li><span><i class="fas fa-star"></i></span></li>
+			  <li><span><i class="fas fa-star"></i></span></li>
+			  <li><span><i class="fas fa-star"></i></span></li>
+			  <li><span><i class="fas fa-star"></i></span></li>
+			  <li class="disable"><span><i class="fas fa-star"></i></span></li>                  
+			</ul>
+		  </div>
+		</div>
+	  </div>
+	</div>
+
+	<div class="testimonial-item">
+	  <div class="row">
+		<div class="col-lg-6">
+		  <div class="testi-img mb-4 mb-lg-0">
+			<img src="img/testimonials/testimonial1.png" alt="">
+		  </div>
+		</div>
+		<div class="col-lg-6">
+		  <div class="testi-right">
+			<h4>Roser Henrique</h4>
+			<p><small>Project Manager, Apple</small></p>
+
+			<p>Waters can not replenish hath fly and be to brought isn't very days behold without land every above lights us fruitful wherein divide it him fowl moving may beginning subdue fly waters can't replenish hath fly and be to brought isn't very days behold </p>
+			<ul class="star_rating mt-3">
+			  <li><span><i class="fas fa-star"></i></span></li>
+			  <li><span><i class="fas fa-star"></i></span></li>
+			  <li><span><i class="fas fa-star"></i></span></li>
+			  <li><span><i class="fas fa-star"></i></span></li>
+			  <li class="disable"><span><i class="fas fa-star"></i></span></li>                  
+			</ul>
+		  </div>
+		</div>
+	  </div>
+	</div>
+  </div>
+</div>
+</section>
+<!--================ End Testimonial Area =================-->
+
+
+
+
+<!--================ Start Portfolio Area =================-->
+<section class="portfolio_area" id="portfolio">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="main_title">
+					<p class="top_text">Our Portfolio <span></span></p>
+					<h2>Check Our Recent <br>
+						Client Work </h2>
+				</div>
+			</div>
+		</div>
+
+		<div class="filters portfolio-filter">
+			<ul>
+				<li class="active" data-filter="*">all</li>
+				<li data-filter=".popular">popular</li>
+				<li data-filter=".latest"> latest</li>
+				<li data-filter=".following">following</li>
+				<li data-filter=".upcoming">upcoming</li>
+			</ul>
+		</div>
+
+		<div class="filters-content">
+			<div class="row portfolio-grid">
+				<div class="grid-sizer col-md-3 col-lg-3"></div>
+				<div class="col-lg-6 col-md-6 all following">
+					<div class="single_portfolio">
+						<img class="img-fluid w-100" src="img/portfolio/p1.jpg" alt="">
+						<div class="overlay"></div>
+						<div class="short_info">
+							<h4><a href="portfolio-details.html">Lens Mockup Design</a></h4>
+							<p>Art, Illustration</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-6 col-md-6 all latest popular upcoming">
+					<div class="single_portfolio">
+						<img class="img-fluid w-100" src="img/portfolio/p4.jpg" alt="">
+						<div class="overlay"></div>
+						<div class="short_info">
+							<h4><a href="portfolio-details.html">Lens Mockup Design</a></h4>
+							<p>Art, Illustration</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-3 col-md-6 all latest following">
+					<div class="single_portfolio">
+						<img class="img-fluid w-100" src="img/portfolio/p2.jpg" alt="">
+						<div class="overlay"></div>
+						<div class="short_info">
+							<h4><a href="portfolio-details.html">Lens Mockup Design</a></h4>
+							<p>Art, Illustration</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-3 col-md-6 all latest upcoming">
+					<div class="single_portfolio">
+						<img class="img-fluid w-100" src="img/portfolio/p3.jpg" alt="">
+						<div class="overlay"></div>
+						<div class="short_info">
+							<h4><a href="portfolio-details.html">Lens Mockup Design</a></h4>
+							<p>Art, Illustration</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-6 col-md-6 all popular">
+					<div class="single_portfolio">
+						<img class="img-fluid w-100" src="img/portfolio/p6.jpg" alt="">
+						<div class="overlay"></div>
+						<div class="short_info">
+							<h4><a href="portfolio-details.html">Lens Mockup Design</a></h4>
+							<p>Art, Illustration</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-6 col-md-6 all popular latest following">
+					<div class="single_portfolio">
+						<img class="img-fluid w-100" src="img/portfolio/p5.jpg" alt="">
+						<div class="overlay"></div>
+						<div class="short_info">
+							<h4><a href="portfolio-details.html">Lens Mockup Design</a></h4>
+							<p>Art, Illustration</p>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+</section>
+<!--================ End Portfolio Area =================-->
+
+
+<!--================ Start Brands Area =================-->
+
+<section class="brands-area section_gap_bottom">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-lg-12">
+				<div class="owl-carousel brand-carousel">
+					<!-- single-brand -->
+					<div class="single-brand-item d-table">
+						<div class="d-table-cell">
+							<img src="img/brands/logo1.png" alt="">
+						</div>
+					</div>
+					<!-- single-brand -->
+					<div class="single-brand-item d-table">
+						<div class="d-table-cell">
+							<img src="img/brands/logo2.png" alt="">
+						</div>
+					</div>
+					<!-- single-brand -->
+					<div class="single-brand-item d-table">
+						<div class="d-table-cell">
+							<img src="img/brands/logo3.png" alt="">
+						</div>
+					</div>
+					<!-- single-brand -->
+					<div class="single-brand-item d-table">
+						<div class="d-table-cell">
+							<img src="img/brands/logo4.png" alt="">
+						</div>
+					</div>
+					<!-- single-brand -->
+					<div class="single-brand-item d-table">
+						<div class="d-table-cell">
+							<img src="img/brands/logo5.png" alt="">
+						</div>
+					</div>
+					<!-- single-brand -->
+					<div class="single-brand-item d-table">
+						<div class="d-table-cell">
+							<img src="img/brands/logo3.png" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!--================ End Brands Area =================-->
+
+<!--================Footer Area =================-->
+<footer class="footer_area">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-lg-12">
+				<div class="footer_top flex-column">
+					<div class="footer_logo">
+						<a href="#">
+							<img src="img/logo2.png" alt="">
+						</a>
+						<div class="d-lg-block d-none">
+							<nav class="navbar navbar-expand-lg navbar-light justify-content-center">
+								<div class="collapse navbar-collapse offset">
+									<ul class="nav navbar-nav menu_nav mx-auto">
+										<li class="nav-item"><a class="nav-link text-white" href="index.html">Home</a></li>
+										<li class="nav-item"><a class="nav-link text-white" href="about.html">About</a></li>
+										<li class="nav-item"><a class="nav-link text-white" href="portfolio.html">Portfolio</a></li>
+										<li class="nav-item"><a class="nav-link text-white" href="blog.html">BLog</a></li>
+										<li class="nav-item"><a class="nav-link text-white" href="services.html">Services </a></li>
+									</ul>
+								</div>
+							</nav>
+						</div>
+					</div>
+					<div class="footer_social mt-lg-0 mt-4">
+						<a href="#"><i class="fab fa-facebook-f"></i></a>
+						<a href="#"><i class="fab fa-twitter"></i></a>
+						<a href="#"><i class="fab fa-skype"></i></a>
+						<a href="#"><i class="fab fa-pinterest-p"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row footer_bottom justify-content-center">
+			<p class="col-lg-8 col-sm-12 footer-text">
+				<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+		</div>
+	</div>
+</footer>
+<!--================End Footer Area =================-->
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="js/popper.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/stellar.js"></script>
+<script src="vendors/isotope/imagesloaded.pkgd.min.js"></script>
+<script src="vendors/isotope/isotope-min.js"></script>
+<script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+<script src="js/jquery.ajaxchimp.min.js"></script>
+<script src="js/mail-script.js"></script>
+<!--gmaps Js-->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script>
+<script src="js/gmaps.min.js"></script>
+<script src="js/theme.js"></script>
+</body>
+
+</html>
