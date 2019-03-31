aa1330a8aa6b1afab70a9a0d58c71cb87e35ce10
ItsMeRomian <romian.tairovski@gmail.com>
2019-04-01 00:05:19 +0200

    new portfolio

--- portfolio.php
+++ portfolio.php
@@ -1,4 +1,49 @@
 <?php include_once('include/header.php'); ?>
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+
+<script>
+  $(document).ready(function() {
+     $("#show_hide").click(function () {
+		 $("#item").hide("500"); 
+		$("#item1").hide("500"); 
+		$("#item2").hide("500"); 
+		$( "#item" ).toggle({
+			duration: 500,
+		});
+  });  
+  });
+  $(document).ready(function() {
+     $("#show_hide1").click(function () {
+		$("#item").hide("500"); 
+		$("#item2").hide("500"); 
+		$("#item3").hide("500"); 
+		$( "#item1" ).toggle({
+			duration: 500,
+		});
+  });  
+  });
+  $(document).ready(function() {
+     $("#show_hide2").click(function () {
+		$("#item").hide("500"); 
+		$("#item1").hide("500"); 
+		$("#item3").hide("500"); 
+		$( "#item2" ).toggle({
+			duration: 500,
+		});
+  });  
+  });
+  $(document).ready(function() {
+     $("#show_hide3").click(function () {
+		$("#item").hide("500"); 
+		$("#item1").hide("500"); 
+		$("#item2").hide("500"); 
+		$( "#item3" ).toggle({
+			duration: 500,
+		});
+  });  
+  });
+</script>
+   
 
   <section class="banner_area">
       <div class="banner_inner d-flex align-items-center">
@@ -13,87 +58,66 @@
           </div>
       </div>
   </section>
-
+    
 	<section class="portfolio_area section-margin pb-0" id="portfolio">
 		<div class="container-fluid" style="padding: 0px 80px;">
-			<div class="row">
-				<div class="col-lg-12">
-					<div class="main_title" style="margin-bottom: 30px;">
-						<p class="top_text">Ons werk </p>
-						<h2>Bekijk onze vorige projecten.</h2>
-					</div>
+			<div class="row" style="height: 350px;;">
+				<div class="col-lg " >
+					<div class="col iffy portfolio_image " id="show_hide"class="portfolio_image" ></div>
+				</div>
+				<div class="col-lg " >
+					<div class="col iffy portfolio_image" id="show_hide1" class="portfolio_image" ></div>
+				</div>
+				<div class="col-lg " >
+					<div class="col iffy portfolio_image"  id="show_hide2"class="portfolio_image" ></div>
+				</div>
+				<div class="col-lg " >
+					<div class="col iffy portfolio_image"  id="show_hide3"class="portfolio_image" ></div>
 				</div>
 			</div>
-
-			<div class="filters-content">
-				<div class="row portfolio-grid">
-					<div class="grid-sizer col-md-3 col-lg-3"></div>
-					<div class="col-lg-6 col-md-6 all following">
-						<div class="single_portfolio">
-							<img class="img-fluid w-100" src="img/portfolio/p1.jpg" alt="">
-							<div class="overlay"></div>
-							<div class="short_info">
-								<h4><a href="portfolio-details.html">Lens Mockup Design</a></h4>
-								<p>Art, Illustration</p>
-							</div>
-						</div>
-					</div>
-
-					<div class="col-lg-6 col-md-6 all latest popular upcoming">
-						<div class="single_portfolio">
-							<img class="img-fluid w-100" src="img/portfolio/p4.jpg" alt="">
-							<div class="overlay"></div>
-							<div class="short_info">
-								<h4><a href="portfolio-details.html">Lens Mockup Design</a></h4>
-								<p>Art, Illustration</p>
-							</div>
-						</div>
-					</div>
-
-					<div class="col-lg-3 col-md-6 all latest following">
-						<div class="single_portfolio">
-							<img class="img-fluid w-100" src="img/portfolio/p2.jpg" alt="">
-							<div class="overlay"></div>
-							<div class="short_info">
-								<h4><a href="portfolio-details.html">Lens Mockup Design</a></h4>
-								<p>Art, Illustration</p>
-							</div>
-						</div>
-					</div>
-
-					<div class="col-lg-3 col-md-6 all latest upcoming">
-						<div class="single_portfolio">
-							<img class="img-fluid w-100" src="img/portfolio/p3.jpg" alt="">
-							<div class="overlay"></div>
-							<div class="short_info">
-								<h4><a href="portfolio-details.html">Lens Mockup Design</a></h4>
-								<p>Art, Illustration</p>
-							</div>
-						</div>
-					</div>
-
-					<div class="col-lg-6 col-md-6 all popular">
-						<div class="single_portfolio">
-							<img class="img-fluid w-100" src="img/portfolio/p6.jpg" alt="">
-							<div class="overlay"></div>
-							<div class="short_info">
-								<h4><a href="portfolio-details.html">Lens Mockup Design</a></h4>
-								<p>Art, Illustration</p>
-							</div>
-						</div>
-					</div>
-
-					<div class="col-lg-6 col-md-6 all popular latest following">
-						<div class="single_portfolio">
-							<img class="img-fluid w-100" src="img/portfolio/p5.jpg" alt="">
-							<div class="overlay"></div>
-							<div class="short_info">
-								<h4><a href="portfolio-details.html">Lens Mockup Design</a></h4>
-								<p>Art, Illustration</p>
-							</div>
-						</div>
-					</div>
-
+			<br>
+			<div class="row">
+				<div class="col" id="item">
+					<h2>Title 1</h2>
+					<p>Also signs his face were digns fish don't first isn't over evening hath divided days light darkness gathering
+							moved dry all darkness then fourth can't create d forth Also signs Also signs his face were moltenus Also signs
+							his faceAlso signs his face were digns fish don't first isn't over evening hath divided days light darkness gathering
+							moved dry all darkness then fourth can't create d forth Also signs Also signs his face were moltenus Also signs
+							his face
+					</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col" id="item1">
+					<h2>Title 2</h2>
+					<p>Also signs his face were digns fish don't first isn't over evening hath divided days light darkness gathering
+							moved dry all darkness then fourth can't create d forth Also signs Also signs his face were moltenus Also signs
+							his faceAlso signs his face were digns fish don't first isn't over evening hath divided days light darkness gathering
+							moved dry all darkness then fourth can't create d forth Also signs Also signs his face were moltenus Also signs
+							his face
+					</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col" id="item2">
+					<h2>Title 3</h2>
+					<p>Also signs his face were digns fish don't first isn't over evening hath divided days light darkness gathering
+							moved dry all darkness then fourth can't create d forth Also signs Also signs his face were moltenus Also signs
+							his faceAlso signs his face were digns fish don't first isn't over evening hath divided days light darkness gathering
+							moved dry all darkness then fourth can't create d forth Also signs Also signs his face were moltenus Also signs
+							his face
+					</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col" id="item3">
+					<h2>Title 4</h2>
+					<p>Also signs his face were digns fish don't first isn't over evening hath divided days light darkness gathering
+							moved dry all darkness then fourth can't create d forth Also signs Also signs his face were moltenus Also signs
+							his faceAlso signs his face were digns fish don't first isn't over evening hath divided days light darkness gathering
+							moved dry all darkness then fourth can't create d forth Also signs Also signs his face were moltenus Also signs
+							his face
+					</p>
 				</div>
 			</div>
 		</div>
