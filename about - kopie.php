91d2020855a422522d380a76c35a025616b33e42
gekketapir <49075147+gekketapir@users.noreply.github.com>
2019-03-31 16:49:12 +0200

    removed services.php (old)

---
+++ about - kopie.php
@@ -0,0 +1,72 @@
+<?php include_once('include/header.php'); ?>
+
+<section class="banner_area section_gap">
+  <div class="banner_inner d-flex align-items-center">
+	  <div class="container">
+		  <div class="banner_content text-center">
+			  <h2>Ons Team</h2>
+			  <div class="page_link">
+				  <a href="index.html">Home</a>
+				  <a href="about.html">Over</a>
+			  </div>
+		  </div>
+	  </div>
+  </div>
+</section>
+
+<section class="about_area section_gap">
+<div class="container">
+	<div class="row justify-content-start align-items-center">
+		<div class="col-lg-5">
+			<div class="about_img">
+				<img class="" src="img/about-us.png" alt="">
+			</div>
+		</div>
+
+		<div class="offset-lg-1 col-lg-5">
+			<div class="main_title text-left" style="margin-top: -85px;">
+				<p class="top_text">ROMIAN TAIROVSKI</p>
+				<h2>
+					Web and Application Developer
+					
+				</h2>
+				<p>
+					Also signs his face were digns fish don't first isn't over evening hath divided days light darkness gathering
+					moved dry all darkness then fourth can't create d forth Also signs Also signs his face were moltenus Also signs
+					his face
+				</p>
+				
+			</div>
+		</div>
+	</div>
+</div>
+</section>
+
+
+<section class="about_area section_gap">
+<div class="container">
+	<div class="row justify-content-start align-items-center">
+		<div class=" col-lg-5">
+			<div class="main_title text-left">
+				<p class="top_text">YOURI PESSINK</p>
+				<h2>
+					Creative Art Director <br>
+					And Designer
+				</h2>
+				<p>
+					Also signs his face were digns fish don't first isn't over evening hath divided days light darkness gathering
+					moved dry all darkness then fourth can't create d forth Also signs Also signs his face were moltenus Also signs
+					his face
+				</p>
+			</div>
+		</div>
+		<div class="col-lg-5">
+			<div class="about_img" style="right: unset;">
+				<img class="" src="img/about-us.png" alt="">
+			</div>
+		</div>
+	</div>
+</div>
+</section>
+
+<?php include_once('include/footer.php'); ?>
