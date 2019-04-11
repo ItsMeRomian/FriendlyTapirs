c0d1e3858cf42044e7db0effdea31d5e3fa8e50e
ItsMeRomian <romian.tairovski@gmail.com>
2019-04-11 12:30:12 +0200

    new portfolio update

--- include/footer.php
+++ include/footer.php
@@ -33,7 +33,7 @@
 		</div>
 	</div>
 </footer>
-<script src="js/jquery-3.2.1.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 <script src="js/popper.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <script src="js/stellar.js"></script>
--- portfolio.php
+++ portfolio.php
@@ -1,74 +1,126 @@
 <?php include_once('include/header.php'); ?>
-<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<section class="banner_area">
+	<div class="banner_inner d-flex align-items-center">
+	  <div class="container">
+		  <div class="banner_content text-center banner_content_h2">
+			  <h2>Portfolio</h2>
+			  <div class="page_link">
+				  <a>ons vorige werk op een rijtje</a>
+			  </div>
+		  </div>
+	  </div>
+	</div>
+</section>
 
-<script>
-  $(document).ready(function() {
-     $("#show_hide").click(function () {
-		 $("#item").hide("500"); 
-		$("#item1").hide("500"); 
-		$("#item2").hide("500"); 
-		$( "#item" ).toggle({
-			duration: 500,
-		});
-  });  
-  });
-  $(document).ready(function() {
-     $("#show_hide1").click(function () {
-		$("#item").hide("500"); 
-		$("#item2").hide("500"); 
-		$("#item3").hide("500"); 
-		$( "#item1" ).toggle({
-			duration: 500,
-		});
-  });  
-  });
-  $(document).ready(function() {
-     $("#show_hide2").click(function () {
-		$("#item").hide("500"); 
-		$("#item1").hide("500"); 
-		$("#item3").hide("500"); 
-		$( "#item2" ).toggle({
-			duration: 500,
-		});
-  });  
-  });
-  $(document).ready(function() {
-     $("#show_hide3").click(function () {
-		$("#item").hide("500"); 
-		$("#item1").hide("500"); 
-		$("#item2").hide("500"); 
-		$( "#item3" ).toggle({
-			duration: 500,
-		});
-  });  
-  });
-</script>
-   
-
-  <section class="banner_area">
-      <div class="banner_inner d-flex align-items-center">
-          <div class="container">
-              <div class="banner_content text-center banner_content_h2">
-                  <h2>Portfolio</h2>
-                  <div class="page_link">
-                      <a>ons vorige werk op een rijtje</a>
-                  </div>
-              </div>
-          </div>
-      </div>
-  </section>
-    
-	<section class="portfolio_area pb-0" id="portfolio" style="padding-top: 40px;">
-		<div class="container-fluid" style="padding: 0px 80px;">
-			<div class="row justify-content-start align-items-center">
-				<div class="col main_title">
-				<h2>Onze vorige projecten.</h2>
-				</div>
-			</div>
-			haha
+<section class="portfolio_area pb-0" id="portfolio" style="padding-top: 40px;">
+	<div class="container-fluid" style="padding: 0px 80px;">
+		<div class="row justify-content-start align-items-center">
+			<div class="col main_title"><h2>Onze vorige projecten.</h2></div>
 		</div>
-	</section>
-	
+		
+		
+<div class="row">
+  <div class="col-lg-4 col-md-12 mb-4">
+    <div class="modal fade" id="modal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+          <div class="modal-body mb-0 p-0">
+            <div class="embed-responsive z-depth-1-half">
+				<img src="img/brands/mockup1.png" style="width: 100%;">
+            </div>
+          </div>
+          <div class="modal-footer justify-content-center">
+            <h2>TipTopIbiza</h2>
+            <p>We’ll be the first to grant you that a full fledged server is going to have more storage space and the capability to do more work (such as transcoding a multi-terabyte video collection in a reasonable span of time). For most people, however, the principle purpose of having an always-on computer somewhere in the house is to serve as a file server and file backup repository. For such tasks the Raspberry Pi is more than powerful enough and will save you a chunk of change in power use.</p>
+            <button type="button" class="float-right btn btn-outline-primary btn-rounded btn-md ml-4" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <a class="portfolio_popup_link">
+		<h3>TipTopIbiza</h3>
+		<img class="bounce05 img-fluid z-depth-1" src="img/brands/mockup1.png" alt="video"
+		data-toggle="modal" data-target="#modal1">
+	</a>
+  </div>
+  
+  <div class="col-lg-4 col-md-12 mb-4">
+    <div class="modal fade" id="modal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+          <div class="modal-body mb-0 p-0">
+            <div class="embed-responsive z-depth-1-half">
+				<img src="img/brands/landing_page_image1.png" style="width: 100%;">
+            </div>
+          </div>
+          <div class="modal-footer justify-content-center">
+            <h2>Autobedrijf Veenstra</h2>
+            <p>We’ll be the first to grant you that a full fledged server is going to have more storage space and the capability to do more work (such as transcoding a multi-terabyte video collection in a reasonable span of time). For most people, however, the principle purpose of having an always-on computer somewhere in the house is to serve as a file server and file backup repository. For such tasks the Raspberry Pi is more than powerful enough and will save you a chunk of change in power use.</p>
+            <button type="button" class="float-right btn btn-outline-primary btn-rounded btn-md ml-4" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <a class="portfolio_popup_link">
+		<h3>Autobedrijf Veenstra</h3>
+		<img class="bounce05 img-fluid z-depth-1" src="img/brands/landing_page_image1.png" alt="video"
+		data-toggle="modal" data-target="#modal2">
+	</a>
+  </div>
+  
+  <div class="col-lg-4 col-md-12 mb-4">
+    <div class="modal fade" id="modal3" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+          <div class="modal-body mb-0 p-0">
+            <div class="embed-responsive z-depth-1-half">
+				<img src="img/brands/landing_page_image1.png" style="width: 100%;">
+            </div>
+          </div>
+          <div class="modal-footer justify-content-center">
+            <h2>OkiTheHandyman</h2>
+            <p>We’ll be the first to grant you that a full fledged server is going to have more storage space and the capability to do more work (such as transcoding a multi-terabyte video collection in a reasonable span of time). For most people, however, the principle purpose of having an always-on computer somewhere in the house is to serve as a file server and file backup repository. For such tasks the Raspberry Pi is more than powerful enough and will save you a chunk of change in power use.</p>
+            <button type="button" class="float-right btn btn-outline-primary btn-rounded btn-md ml-4" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <a class="portfolio_popup_link">
+		<h3>OkiTheHandyman</h3>
+		<img class="bounce05 img-fluid z-depth-1" src="img/brands/landing_page_image1.png" alt="video"
+		data-toggle="modal" data-target="#modal3">
+	</a>
+  </div>
+  
+  <div class="col-lg-4 col-md-12 mb-4">
+    <div class="modal fade" id="modal4" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+          <div class="modal-body mb-0 p-0">
+            <div class="embed-responsive z-depth-1-half">
+				<img src="img/brands/landing_page_image1.png" style="width: 100%;">
+            </div>
+          </div>
+          <div class="modal-footer justify-content-center">
+            <h2>DynaHotel</h2>
+            <p>We’ll be the first to grant you that a full fledged server is going to have more storage space and the capability to do more work (such as transcoding a multi-terabyte video collection in a reasonable span of time). For most people, however, the principle purpose of having an always-on computer somewhere in the house is to serve as a file server and file backup repository. For such tasks the Raspberry Pi is more than powerful enough and will save you a chunk of change in power use.</p>
+            <button type="button" class="float-right btn btn-outline-primary btn-rounded btn-md ml-4" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <a class="portfolio_popup_link">
+		<h3>DynaHotel</h3>
+		<img class="bounce05 img-fluid z-depth-1" src="img/brands/landing_page_image1.png" alt="video"
+		data-toggle="modal" data-target="#modal4">
+	</a>
+  </div>
+  
+  
+</div>
+</div>
+</section>
+
 
 
 
